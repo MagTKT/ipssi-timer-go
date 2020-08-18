06c99b4460a069dc2f234dc7d42a2264f1440188
magali gosselin <magali@wertkt.com>
2020-08-18 15:16:53 +0200

    change form from user for repeat password

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,14 +6,17 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\{EmailType, PasswordType, RepeatedType, SubmitType, TextType};
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+            ])
         ;
     }
 
