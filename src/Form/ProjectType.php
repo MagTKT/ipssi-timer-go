879b8c5db06ff7d6e1ec67de93b1464ef737f76b
magali gosselin <magali@wertkt.com>
2020-08-19 12:24:51 +0200

    change form frome projet

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -6,13 +6,17 @@ use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\{TextType, SubmitType,};
 
 class ProjectType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name_project')
+            ->add('name_project', TextType::class, ['label' => 'Nom'])
+            ->add('save', SubmitType::class,[
+                'label' => 'Envoyer',
+            ])
         ;
     }
 
