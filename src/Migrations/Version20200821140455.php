d98f11bb87442aa5e189fb170e19e467069ef921
gregory <gregory.dossantos98@gmail.com>
2020-08-21 16:20:48 +0200

    dernière migration

--- src/Migrations/Version20200821140455.php
+++ src/Migrations/Version20200821140455.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200821090428 extends AbstractMigration
+final class Version20200821140455 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200821090428 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, id_status_id INT DEFAULT NULL, name_project VARCHAR(255) NOT NULL, date_creation DATETIME DEFAULT NULL, INDEX IDX_2FB3D0EEEBC2BC9A (id_status_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, id_status_id INT DEFAULT NULL, team_id INT DEFAULT NULL, name_project VARCHAR(255) NOT NULL, date_creation DATETIME DEFAULT NULL, INDEX IDX_2FB3D0EEEBC2BC9A (id_status_id), INDEX IDX_2FB3D0EE296CD8AE (team_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE team (id INT AUTO_INCREMENT NOT NULL, id_status_id INT DEFAULT NULL, team_admin_id INT NOT NULL, name VARCHAR(255) NOT NULL, date_creation DATETIME DEFAULT NULL, INDEX IDX_C4E0A61FEBC2BC9A (id_status_id), INDEX IDX_C4E0A61FDC695E6E (team_admin_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, status_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, last_name VARCHAR(255) DEFAULT NULL, date_creation DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D6496BF700BD (status_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -30,6 +30,7 @@ final class Version20200821090428 extends AbstractMigration
         $this->addSql('CREATE TABLE user_project (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, id_project_id INT NOT NULL, id_status_id INT DEFAULT NULL, date_creation DATETIME DEFAULT NULL, INDEX IDX_77BECEE479F37AE5 (id_user_id), INDEX IDX_77BECEE4B3E79F4B (id_project_id), INDEX IDX_77BECEE4EBC2BC9A (id_status_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_team (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, id_team_id INT NOT NULL, id_status_id INT DEFAULT NULL, date_creation DATETIME DEFAULT NULL, INDEX IDX_BE61EAD679F37AE5 (id_user_id), INDEX IDX_BE61EAD6F7F171DE (id_team_id), INDEX IDX_BE61EAD6EBC2BC9A (id_status_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EEEBC2BC9A FOREIGN KEY (id_status_id) REFERENCES status (id)');
+        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EE296CD8AE FOREIGN KEY (team_id) REFERENCES team (id)');
         $this->addSql('ALTER TABLE team ADD CONSTRAINT FK_C4E0A61FEBC2BC9A FOREIGN KEY (id_status_id) REFERENCES status (id)');
         $this->addSql('ALTER TABLE team ADD CONSTRAINT FK_C4E0A61FDC695E6E FOREIGN KEY (team_admin_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6496BF700BD FOREIGN KEY (status_id) REFERENCES status (id)');
@@ -51,6 +52,7 @@ final class Version20200821090428 extends AbstractMigration
 
         $this->addSql('ALTER TABLE timer DROP FOREIGN KEY FK_6AD0DE1AB3E79F4B');
         $this->addSql('ALTER TABLE user_project DROP FOREIGN KEY FK_77BECEE4B3E79F4B');
+        $this->addSql('ALTER TABLE project DROP FOREIGN KEY FK_2FB3D0EE296CD8AE');
         $this->addSql('ALTER TABLE timer DROP FOREIGN KEY FK_6AD0DE1AF7F171DE');
         $this->addSql('ALTER TABLE user_team DROP FOREIGN KEY FK_BE61EAD6F7F171DE');
         $this->addSql('ALTER TABLE team DROP FOREIGN KEY FK_C4E0A61FDC695E6E');
