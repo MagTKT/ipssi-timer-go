1dcef6c5a46abbf4e25a4e3616eea61bfa408103
magali gosselin <magali@wertkt.com>
2020-08-24 08:35:57 +0200

    create template from timer and register timer

--- src/Controller/TimerController.php
+++ src/Controller/TimerController.php
@@ -27,48 +27,13 @@ class TimerController extends AbstractController
     }
 
     /**
-    * @Route("/projects/{id}/timers", name="timer")
-    */
-    // public function createTimer(Request $request, int $id)
-    // {
-    //     $content = json_decode($request->getContent(), true);
-    //     $project = $this->projectRepository->find($id);
-    //     $timer = new Timer();
-    //     $timer->setidUser($this->getidUser());
-    //     $timer->setidTeam($this->getidTeam());
-    //     $timer->setidProject($project);
-    //     $timer->setDatetime_Debut(new \DateTime());
-    //     $timer->setDatetime_Fin(new \DateTime());
-    //     $timer->setCumul_s($this->getCumul_s());
-    //     $timer->setTimerComment($this->getTimerComment());
-    //     $this->updateDatabase($timer);
-    //     // Serialize object into Json format
-    //     $jsonContent = $this->serializeObject($timer);
-    //     return new Response($jsonContent, Response::HTTP_OK);
-    // }
-    
-    /**
-    * @Route("/project/timers/active", name="active_timer")
-    */
-    // public function runningTimer()
-    // {
-    //     $timer = $this->timerRepository->findRunningTimer($this->getidUser()->getId());
-    //     $jsonContent = $this->serializeObject($timer);
-    //     return new Response($jsonContent, Response::HTTP_OK);
-    // }
-
-    /**
-    * @Route("/projects/{id}/timers/stop", name="stop_running")
-    */
-//    public function stopRunningTimer()
-//    {
-//        $timer = $this->timerRepository->findRunningTimer($this->getidUser()->getId());
-//        if ($timer) {
-//            $timer->setDateTime_fin(new \DateTime());
-//            $this->updateDatabase($timer);
-//        }
-//        // Serialize object into Json format
-//        $jsonContent = $this->serializeObject($timer);
-//        return new Response($jsonContent, Response::HTTP_OK);
-//    }
+     * @Route("/new", name="timer_new", methods={"GET"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     */
+    public function new(TimerRepository $timerRepository): Response
+    {
+        return $this->render('timer/new.html.twig', [
+            'timer' => $timerRepository->findAll(),
+        ]);
+    }
 }
